b7264e0c4d12a66ce7042569e54dde354af72f9f
David P Sanders <davepaulsanders@gmail.com>
2021-03-27 19:32:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,5 +23,6 @@ function doWhileLoop(num){
     num = num + 1;
     return num
   }
-do console.log("I run once regardless.")
+do {console.log("I run once regardless.")
 } while incrementVariable() <10)
+}
